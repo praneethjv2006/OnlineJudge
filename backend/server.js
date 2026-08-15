a02912f3a11bd438c3195daf674e371f4de80f62
J V Praneeth <2023mcb1296@iitrpr.ac.in>
2026-08-15 21:51:47 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,7 @@ const allowedOrigins = [
   process.env.CLIENT_URL,
   "http://localhost:5173",
   "http://localhost:3000",
+  "https://onlinejudge-xtob.onrender.com",
 ].filter(Boolean);
 
 app.use(
